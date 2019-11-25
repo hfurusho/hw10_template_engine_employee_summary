80e821bee86e4123bf6cd2fedd48bc16e28934c2
Kami <51175640+hfurusho@users.noreply.github.com>
2019-11-24 21:47:02 -0800

    All questions built and buildTeam function works properly.

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const Manager = require("./lib/Manager.js");
-const Enginer = require("./lib/Engineer.js");
+const Engineer = require("./lib/Engineer.js");
 const Intern = require("./lib/Intern.js");
 const questions = require("./lib/questions.js");
 const inquirer = require("inquirer");
+
+const sampleTeam = [ Manager {
+    name: 'joe',
+    id: 121,
+    email: 'something@something',
+    officeNumber: 555 },
+  Engineer { name: 'Haps', id: 432, email: 'anotherone', github: 'hfurusho' },
+  Intern { name: 'smaller', id: 678, email: 'joey@sb.com', school: 'UCLA' }]
+  
 init();
 
 async function init() {
@@ -30,15 +39,41 @@ async function buildTeam() {
   while (buildingTeam) {
     if (!manager) {
       manager = await inquirer.prompt(questions.manager);
-      team.push(manager);
+      team.push(
+        new Manager(
+          manager.name,
+          manager.idNumber,
+          manager.email,
+          manager.uniqueProp
+        )
+      );
     }
+
     let nextMember = await inquirer.prompt(questions.nextMember);
+
     if (nextMember.position === "engineer") {
       let engineer = await inquirer.prompt(questions.engineer);
-      team.push(engineer);
+      team.push(
+        new Engineer(
+          engineer.name,
+          engineer.idNumber,
+          engineer.email,
+          engineer.uniqueProp
+        )
+      );
     } else if (nextMember.position === "intern") {
+      let intern = await inquirer.prompt(questions.intern);
+      team.push(
+        new Intern(
+          intern.name,
+          intern.idNumber,
+          intern.email,
+          intern.uniqueProp
+        )
+      );
     } else {
       buildingTeam = false;
+      console.log(team);
     }
   }
 }
